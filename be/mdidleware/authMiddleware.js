82566184294237294893a5950a217457c9b26ecb
Ayo Ashiru <ashiruayo@gmail.com>
2023-09-29 17:38:06 +0100

    add comment to authMiddleware

--- be/mdidleware/authMiddleware.js
+++ be/mdidleware/authMiddleware.js
@@ -3,19 +3,22 @@ const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
 const protectMiddleware = asyncHandler(async (req, res, next) => {
+  // check for token
   const token = req.cookies.token;
   if (!token) {
     res.status(401);
     throw new Error('unauthorized request');
   }
 
-  // verify token
+  // verify token.
   const isTokenVerified = jwt.verify(token, process.env.JWT_SECRET);
+  // variable return three props(id,iat,exp)
 
   // get userID from token
-  const userId = await User.findById(isTokenVerified.id).select('-password');
-  if (isTokenVerified && userId) {
-    req.user = userId;
+  const userObj = await User.findById(isTokenVerified.id).select('-password');
+  if (isTokenVerified && userObj) {
+    // add user object to req
+    req.user = userObj;
     next();
   } else {
     res.status(401).send('user not authorized!');
